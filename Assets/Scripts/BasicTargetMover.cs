ef32027a7b28568a19da303c61b63408802ca558
Chenade <chenade0312@gmail.com>
2021-02-26 12:23:16 +0800

    v 1.0.0

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    public bool isSpin = true;
+    public float spinSpped = 180.0f;
+
+    public bool isMotion = false;
+    public float motionMagnitude = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpin)
+        {
+            transform.Rotate(Vector3.up * spinSpped * Time.deltaTime);
+        }
+
+        if (isMotion)
+        {
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+    }
+}
